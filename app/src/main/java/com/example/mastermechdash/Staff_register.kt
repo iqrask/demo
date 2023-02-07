7046baad6a28380ea330d0ea6da75cfc14f2d35d
iqra <iqrashakkh.59@gmail.com>
2023-02-07 16:10:45 +0530

    first commit

---
+++ app/src/main/java/com/example/mastermechdash/Staff_register.kt
@@ -0,0 +1,11 @@
+package com.example.mastermechdash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Staff_register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_register)
+    }
+}
